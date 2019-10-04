ce2b3fb599aae818ab32499ec8b81c78a2ec8b4c
Witold Kręcicki <wpk@isc.org>
2019-10-04 14:38:47 +0200

    hp: Can't use const atomics

--- lib/isc/hp.c
+++ lib/isc/hp.c
@@ -10,6 +10,8 @@
  */
 
 /*
+ * Hazard Pointer implementation.
+ *
  * This work is based on C++ code available from:
  * https://github.com/pramalhe/ConcurrencyFreaks/
  *
@@ -18,6 +20,7 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -27,17 +30,17 @@
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER>
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <inttypes.h>
@@ -49,21 +52,25 @@
 #include <isc/util.h>
 
 #define HP_MAX_THREADS 128
-#define HP_MAX_HPS 4 /* This is named 'K' in the HP paper */
+#define HP_MAX_HPS 4			/* This is named 'K' in the HP paper */
 #define CLPAD (128 / sizeof(uintptr_t))
-#define HP_THRESHOLD_R 0 /* This is named 'R' in the HP paper */
-#define MAX_RETIRED (HP_MAX_THREADS * HP_MAX_HPS) /* Maximum number of retired objects per thread */
+#define HP_THRESHOLD_R 0		/* This is named 'R' in the HP paper */
+
+/* Maximum number of retired objects per thread */
+#define MAX_RETIRED (HP_MAX_THREADS * HP_MAX_HPS)
 
 #define TID_UNKNOWN -1
-static atomic_int _tid_v_base = 0;
+
+static atomic_int tid_v_base = 0;
+
 #if defined(HAVE_TLS)
 #if defined(HAVE_THREAD_LOCAL)
 #include <threads.h>
-static thread_local int _tid_v = TID_UNKNOWN;
+static thread_local int tid_v = TID_UNKNOWN;
 #elif defined(HAVE___THREAD)
-static __thread int _tid_v = TID_UNKNOWN;
+static __thread int tid_v = TID_UNKNOWN;
 #elif defined(HAVE___DECLSPEC_THREAD)
-static __declspec( thread ) int _tid_v = TID_UNKNOWN;
+static __declspec( thread ) int tid_v = TID_UNKNOWN;
 #else  /* if defined(HAVE_THREAD_LOCAL) */
 #error "Unknown method for defining a TLS variable!"
 #endif /* if defined(HAVE_THREAD_LOCAL) */
@@ -71,128 +78,145 @@ static __declspec( thread ) int _tid_v = TID_UNKNOWN;
 #error "Thread-local storage support is required!"
 #endif /* if defined(HAVE_TLS) */
 
-struct isc__hp_rl {
-	int size;
-	uintptr_t list[MAX_RETIRED];
-};
+typedef struct retirelist {
+	int			size;
+	uintptr_t		list[MAX_RETIRED];
+} retirelist_t;
 
 struct isc_hp {
-	int max_hps;
-	isc_mem_t *mctx;
-	atomic_uintptr_t *hp[HP_MAX_THREADS];
-	struct isc__hp_rl *rl[HP_MAX_THREADS];
-	isc_hp_deletefunc_t *deletefunc;
+	int			max_hps;
+	isc_mem_t		*mctx;
+	atomic_uintptr_t	*hp[HP_MAX_THREADS];
+	retirelist_t		*rl[HP_MAX_THREADS];
+	isc_hp_deletefunc_t	*deletefunc;
 };
 
 static inline int
-_tid() {
-	if (_tid_v == TID_UNKNOWN) {
-		_tid_v = atomic_fetch_add(&_tid_v_base, 1);
-		REQUIRE(_tid_v < HP_MAX_THREADS);
+tid() {
+	if (tid_v == TID_UNKNOWN) {
+		tid_v = atomic_fetch_add(&tid_v_base, 1);
+		REQUIRE(tid_v < HP_MAX_THREADS);
 	}
-	return (_tid_v);
+
+	return (tid_v);
 }
 
 isc_hp_t *
 isc_hp_new(isc_mem_t *mctx, size_t max_hps, isc_hp_deletefunc_t *deletefunc) {
 	isc_hp_t *hp = isc_mem_get(mctx, sizeof(*hp));
+
 	if (max_hps == 0) {
 		max_hps = HP_MAX_HPS;
 	}
-	*hp = (isc_hp_t){ .max_hps = max_hps,
-			  .deletefunc = deletefunc };
+
+	*hp = (isc_hp_t){
+		.max_hps = max_hps,
+		.deletefunc = deletefunc
+	};
+
 	isc_mem_attach(mctx, &hp->mctx);
 
 	for (int i = 0; i < HP_MAX_THREADS; i++) {
 		hp->hp[i] = isc_mem_get(mctx, CLPAD * 2 * sizeof(hp->hp[i][0]));
 		hp->rl[i] = isc_mem_get(mctx, sizeof(*hp->rl[0]));
-		*hp->rl[i] = (struct isc__hp_rl) { .size = 0 };
+		*hp->rl[i] = (retirelist_t) { .size = 0 };
+
 		for (int j = 0; j < hp->max_hps; j++) {
 			atomic_init(&hp->hp[i][j], 0);
 		}
 	}
+
 	return (hp);
 }
 
 void
 isc_hp_destroy(isc_hp_t *hp) {
 	for (int i = 0; i < HP_MAX_THREADS; i++) {
-		isc_mem_put(hp->mctx, hp->hp[i], CLPAD * 2 * sizeof(uintptr_t));
+		isc_mem_put(hp->mctx, hp->hp[i],
+			    CLPAD * 2 * sizeof(uintptr_t));
 
 		for (int j = 0; j < hp->rl[i]->size; j++) {
 			void *data = (void *)hp->rl[i]->list[j];
 			hp->deletefunc(data);
 		}
+
 		isc_mem_put(hp->mctx, hp->rl[i], sizeof(*hp->rl[0]));
 	}
+
 	isc_mem_putanddetach(&hp->mctx, hp, sizeof(*hp));
 }
 
 void
 isc_hp_clear(isc_hp_t *hp) {
 	for (int i = 0; i < hp->max_hps; i++) {
-		atomic_store_release(&hp->hp[_tid()][i], 0);
+		atomic_store_release(&hp->hp[tid()][i], 0);
 	}
 }
 
-
-/**
+/*
  * Progress Condition: wait-free population oblivious
  */
 void isc_hp_clear_one(isc_hp_t *hp, int ihp) {
-	atomic_store_release(&hp->hp[_tid()][ihp], 0);
+	atomic_store_release(&hp->hp[tid()][ihp], 0);
 }
 
-/**
+/*
  * Progress Condition: lock-free
  */
 uintptr_t
-isc_hp_protect(isc_hp_t *hp, int ihp, const atomic_uintptr_t* atom) {
+isc_hp_protect(isc_hp_t *hp, int ihp, atomic_uintptr_t* atom) {
 	uintptr_t n = 0;
 	uintptr_t ret;
 	while ((ret = atomic_load(atom)) != n) {
-		atomic_store(&hp->hp[_tid()][ihp], ret);
+		atomic_store(&hp->hp[tid()][ihp], ret);
 		n = ret;
 	}
 	return (ret);
 }
 
-/**
- * This returns the same value that is passed as ptr, which is sometimes useful
+/*
+ * This returns the same value that is passed as ptr, which is sometimes
+ * useful.
+ *
  * Progress Condition: wait-free population oblivious
  */
 uintptr_t
 isc_hp_protect_ptr(isc_hp_t *hp, int ihp, const atomic_uintptr_t ptr) {
-	atomic_store(&hp->hp[_tid()][ihp], ptr);
+	atomic_store(&hp->hp[tid()][ihp], ptr);
 	return (ptr);
 }
 
-/**
- * This returns the same value that is passed as ptr, which is sometimes useful
+/*
+ * This returns the same value that is passed as ptr, which is sometimes
+ * useful.
+ *
  * Progress Condition: wait-free population oblivious
  */
 uintptr_t
 isc_hp_protect_release(isc_hp_t *hp, int ihp, const atomic_uintptr_t ptr) {
-	atomic_store_release(&hp->hp[_tid()][ihp], ptr);
+	atomic_store_release(&hp->hp[tid()][ihp], ptr);
 	return (ptr);
 }
 
-/**
+/*
  * Progress Condition: wait-free bounded (by the number of threads squared)
  */
 void
 isc_hp_retire(isc_hp_t *hp, const atomic_uintptr_t ptr) {
-	hp->rl[_tid()]->list[hp->rl[_tid()]->size++] = ptr;
-	INSIST(hp->rl[_tid()]->size < MAX_RETIRED);
+	hp->rl[tid()]->list[hp->rl[tid()]->size++] = ptr;
+	INSIST(hp->rl[tid()]->size < MAX_RETIRED);
 
-	if (hp->rl[_tid()]->size < HP_THRESHOLD_R) {
+	if (hp->rl[tid()]->size < HP_THRESHOLD_R) {
 		return;
 	}
 
-	for (int iret = 0; iret < hp->rl[_tid()]->size;) {
-		uintptr_t obj = hp->rl[_tid()]->list[iret];
+	for (int iret = 0; iret < hp->rl[tid()]->size;) {
+		uintptr_t obj = hp->rl[tid()]->list[iret];
 		bool can_delete = true;
-		for (int itid = 0; itid < HP_MAX_THREADS && can_delete; itid++) {
+		for (int itid = 0;
+		     itid < HP_MAX_THREADS && can_delete;
+		     itid++)
+		{
 			for (int ihp = hp->max_hps-1; ihp >= 0; ihp--) {
 				if (atomic_load(&hp->hp[itid][ihp]) == obj) {
 					can_delete = false;
@@ -200,15 +224,17 @@ isc_hp_retire(isc_hp_t *hp, const atomic_uintptr_t ptr) {
 				}
 			}
 		}
+
 		if (can_delete) {
-			size_t bytes = (hp->rl[_tid()]->size - iret) *
-				sizeof(hp->rl[_tid()]->list[0]);
-			memmove(&hp->rl[_tid()]->list[iret],
-				&hp->rl[_tid()]->list[iret + 1],
+			size_t bytes = (hp->rl[tid()]->size - iret) *
+				sizeof(hp->rl[tid()]->list[0]);
+			memmove(&hp->rl[tid()]->list[iret],
+				&hp->rl[tid()]->list[iret + 1],
 				bytes);
-			hp->rl[_tid()]->size--;
+			hp->rl[tid()]->size--;
 			hp->deletefunc((void *)obj);
 		}
+
 		iret++;
 	}
 }
--- lib/isc/include/isc/hp.h
+++ lib/isc/include/isc/hp.h
@@ -10,6 +10,8 @@
  */
 
 /*
+ * Hazard Pointer implementation.
+ *
  * This work is based on C++ code available from:
  * https://github.com/pramalhe/ConcurrencyFreaks/
  *
@@ -18,6 +20,7 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -27,28 +30,27 @@
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER>
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <isc/atomic.h>
 #include <isc/string.h>
 #include <isc/mem.h>
+#include <isc/types.h>
 #include <isc/util.h>
 
 typedef void
 (isc_hp_deletefunc_t)(void *);
-
-typedef struct isc_hp isc_hp_t;
 
 isc_hp_t *
 isc_hp_new(isc_mem_t *mctx, size_t max_hps, isc_hp_deletefunc_t *deletefunc);
@@ -59,10 +61,11 @@ isc_hp_destroy(isc_hp_t *hp);
 void
 isc_hp_clear(isc_hp_t *hp);
 
-void isc_hp_clear_one(isc_hp_t *hp, int ihp);
+void
+isc_hp_clear_one(isc_hp_t *hp, int ihp);
 
 uintptr_t
-isc_hp_protect(isc_hp_t *hp, int ihp, const atomic_uintptr_t* atom);
+isc_hp_protect(isc_hp_t *hp, int ihp, atomic_uintptr_t* atom);
 
 uintptr_t
 isc_hp_protect_ptr(isc_hp_t *hp, int ihp, const atomic_uintptr_t ptr);
