356c73d2dae660e390d957a602487f769a03ff29
Witold Kręcicki <wpk@isc.org>
2019-10-18 16:40:41 +0200

    Free all extra data in ns_client_t in client_put_cb

--- lib/ns/client.c
+++ lib/ns/client.c
@@ -1581,10 +1581,6 @@ client_put_cb(void *client0) {
 		ISC_QUEUE_UNLINK(client->manager->inactive, client, ilink);
 	}
 
-	if (client->task != NULL) {
-		isc_task_shutdown(client->task);
-	}
-
 	client->newstate = NS_CLIENTSTATE_FREED;
 	if (client->handle != NULL) {
 		isc_nmhandle_detach(&client->handle);
@@ -1592,6 +1588,54 @@ client_put_cb(void *client0) {
 
 	if (client->manager != NULL) {
 		clientmgr_detach(&client->manager);
+	}
+
+	ns_query_free(client);
+	isc_mem_put(client->mctx, client->recvbuf, NS_CLIENT_RECV_BUFFER_SIZE);
+	if (client->delaytimer != NULL) {
+		isc_timer_detach(&client->delaytimer);
+	}
+	if (client->tcpbuf != NULL) {
+		isc_mem_put(client->mctx, client->tcpbuf,
+			    NS_CLIENT_TCP_BUFFER_SIZE);
+	}
+	if (client->opt != NULL) {
+		INSIST(dns_rdataset_isassociated(client->opt));
+		dns_rdataset_disassociate(client->opt);
+		dns_message_puttemprdataset(client->message,
+					    &client->opt);
+	}
+	if (client->keytag != NULL) {
+		isc_mem_put(client->mctx, client->keytag,
+			    client->keytag_len);
+		client->keytag_len = 0;
+	}
+
+	dns_message_destroy(&client->message);
+
+	/*
+	 * Detaching the task must be done after unlinking from
+	 * the manager's lists because the manager accesses
+	 * client->task.
+	 */
+	if (client->task != NULL) {
+		isc_task_detach(&client->task);
+	}
+
+	client->magic = 0;
+
+	/*
+	 * Destroy the fetchlock mutex that was created in
+	 * ns_query_init().
+	 */
+	isc_mutex_destroy(&client->query.fetchlock);
+
+	if (client->sctx != NULL) {
+		ns_server_detach(&client->sctx);
+	}
+
+	if (client->mctx != NULL) {
+		isc_mem_detach(&client->mctx);
 	}
 }
 
