0246b46f0679f5e703f976f50a1ef0397ca5b971
Witold Kręcicki <wpk@isc.org>
2019-10-23 23:11:15 +0200

    netmgr test: mutex (and not atomics) based astack

--- lib/isc/astack.c
+++ lib/isc/astack.c
@@ -15,14 +15,16 @@
 #include <isc/astack.h>
 #include <isc/atomic.h>
 #include <isc/mem.h>
+#include <isc/mutex.h>
 #include <isc/types.h>
 #include <isc/util.h>
 
 struct isc_astack {
 	isc_mem_t *mctx;
 	size_t size;
-	atomic_uint_fast32_t current;
-	_Atomic uintptr_t nodes[];
+	size_t pos;
+	isc_mutex_t lock;
+	uintptr_t nodes[];
 };
 
 isc_astack_t *
@@ -34,61 +36,43 @@ isc_astack_new(isc_mem_t *mctx, size_t size) {
 	stack->mctx = NULL;
 	isc_mem_attach(mctx, &stack->mctx);
 	stack->size = size;
-	atomic_init(&stack->current, 0);
+	stack->pos = 0;
 	memset(stack->nodes, 0, size * sizeof(uintptr_t));
+	isc_mutex_init(&stack->lock);
 	return (stack);
 }
 
 bool
 isc_astack_trypush(isc_astack_t *stack, void *obj) {
-	atomic_uint_fast32_t cur = atomic_load(&stack->current);
-	uintptr_t v = 0;
-
-	if (cur >= stack->size) {
-		return (false);
-	}
-
-	/* We first add the value to the list */
-	if (atomic_compare_exchange_strong(&stack->nodes[cur], &v,
-					   (uintptr_t) obj))
-	{
-		/* Success, we can update cur */
-		atomic_fetch_add(&stack->current, 1);
+	if (isc_mutex_trylock(&stack->lock) == false) {
+		if (stack->pos >= stack->size) {
+			isc_mutex_unlock(&stack->lock);
+			return (false);
+		}
+		stack->nodes[stack->pos++] = (uintptr_t) obj;
+		isc_mutex_unlock(&stack->lock);
 		return (true);
 	} else {
-		/* Failure, bail */
 		return (false);
 	}
 }
 
 void *
 isc_astack_pop(isc_astack_t *stack) {
-	uintptr_t nv = 0;
-
-	while (true) {
-		uint_fast32_t cur = atomic_load(&stack->current);
-		uint_fast32_t next;
-
-		if (cur == 0) {
-			return (NULL);
-		}
-
-		next = cur - 1;
-
-		if (atomic_compare_exchange_strong(&stack->current,
-						   &cur, next))
-		{
-			void *obj =
-				(void *) atomic_exchange(&stack->nodes[next],
-							 nv);
-			return (obj);
-		}
+	isc_mutex_lock(&stack->lock);
+	uintptr_t rv;
+	if (stack->pos == 0) {
+		rv = 0;
+	} else {
+		rv = stack->nodes[--stack->pos];
 	}
+	isc_mutex_unlock(&stack->lock);
+	return ((void*) rv);
 }
 
 void
 isc_astack_destroy(isc_astack_t *stack) {
-	REQUIRE(stack->current == 0);
+	REQUIRE(stack->pos == 0);
 
 	isc_mem_putanddetach(&stack->mctx, stack,
 			     sizeof(struct isc_astack) +
