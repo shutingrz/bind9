5c4917f66e39ff03be79d3e7d5d2714dffc9cc90
Mark Andrews <marka@isc.org>
2020-02-03 16:45:15 +1100

    remove unused label

--- lib/dns/client.c
+++ lib/dns/client.c
@@ -1775,14 +1775,11 @@ dns_client_startrequest(dns_client_t *client, dns_message_t *qmessage,
 
 	isc_refcount_decrement(&client->references);
 
- cleanup:
-	if (ctx != NULL) {
-		LOCK(&client->lock);
-		ISC_LIST_UNLINK(client->reqctxs, ctx, link);
-		UNLOCK(&client->lock);
-		isc_mutex_destroy(&ctx->lock);
-		isc_mem_put(client->mctx, ctx, sizeof(*ctx));
-	}
+	LOCK(&client->lock);
+	ISC_LIST_UNLINK(client->reqctxs, ctx, link);
+	UNLOCK(&client->lock);
+	isc_mutex_destroy(&ctx->lock);
+	isc_mem_put(client->mctx, ctx, sizeof(*ctx));
 	if (event != NULL)
 		isc_event_free(ISC_EVENT_PTR(&event));
 	isc_task_detach(&tclone);
