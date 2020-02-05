84eb7ee07e687f6f4c5873004ed19fc907f3e515
Ondřej Surý <ondrej@isc.org>
2020-02-05 18:34:55 +0100

    Refactor isc_rwlock to use platform native rwlock APIs
    This merge request changes the isc_rwlock to use pthread_rwlock API on
    POSIX platforms, and Slim Reader/Writer (SRW) Locks on Windows.
    
    The isc_rwlock API itself has been simplified as result of the change:
    
    - isc_rwlock_init, isc_rwlock_destroy, isc_rwlock_lock, and
      isc_rwlock_unlock now return (void) as they cannot fail gracefully.
    - isc_rwlock_tryupgrade has been dropped as it cannot be implemented
      using native APIs

--- lib/isc/include/isc/rwlock.h
+++ lib/isc/include/isc/rwlock.h
@@ -9,12 +9,16 @@
  * information regarding copyright ownership.
  */
 
-#ifndef ISC_RWLOCK_H
-#define ISC_RWLOCK_H 1
-
-#include <inttypes.h>
+#pragma once
 
 /*! \file isc/rwlock.h */
+
+#include <inttypes.h>
+#ifndef _WIN32
+#include <pthread.h>
+#else
+#include <synchapi.h>
+#endif
 
 #include <isc/atomic.h>
 #include <isc/condition.h>
@@ -30,69 +34,31 @@ typedef enum {
 	isc_rwlocktype_write
 } isc_rwlocktype_t;
 
-#if USE_PTHREAD_RWLOCK
+#ifndef _WIN32
 #include <pthread.h>
-
 struct isc_rwlock {
 	pthread_rwlock_t rwlock;
 	atomic_bool	 downgrade;
 };
-
-#else /* USE_PTHREAD_RWLOCK */
-
+#else
+#include <windows.h>
 struct isc_rwlock {
-	/* Unlocked. */
-	unsigned int	    magic;
-	isc_mutex_t	    lock;
-	atomic_int_fast32_t spins;
-
-	/*
-	 * When some atomic instructions with hardware assistance are
-	 * available, rwlock will use those so that concurrent readers do not
-	 * interfere with each other through mutex as long as no writers
-	 * appear, massively reducing the lock overhead in the typical case.
-	 *
-	 * The basic algorithm of this approach is the "simple
-	 * writer-preference lock" shown in the following URL:
-	 * http://www.cs.rochester.edu/u/scott/synchronization/pseudocode/rw.html
-	 * but our implementation does not rely on the spin lock unlike the
-	 * original algorithm to be more portable as a user space application.
-	 */
-
-	/* Read or modified atomically. */
-	atomic_int_fast32_t write_requests;
-	atomic_int_fast32_t write_completions;
-	atomic_int_fast32_t cnt_and_flag;
-
-	/* Locked by lock. */
-	isc_condition_t readable;
-	isc_condition_t writeable;
-	unsigned int	readers_waiting;
-
-	/* Locked by rwlock itself. */
-	atomic_uint_fast32_t write_granted;
-
-	/* Unlocked. */
-	unsigned int write_quota;
+	PSRWLOCK    rwlock;
+	atomic_bool downgrade;
 };
+#endif
 
-#endif /* USE_PTHREAD_RWLOCK */
+void
+isc_rwlock_init(isc_rwlock_t *rwl);
 
-isc_result_t
-isc_rwlock_init(isc_rwlock_t *rwl, unsigned int read_quota,
-		unsigned int write_quota);
-
-isc_result_t
+void
 isc_rwlock_lock(isc_rwlock_t *rwl, isc_rwlocktype_t type);
 
 isc_result_t
 isc_rwlock_trylock(isc_rwlock_t *rwl, isc_rwlocktype_t type);
 
-isc_result_t
+void
 isc_rwlock_unlock(isc_rwlock_t *rwl, isc_rwlocktype_t type);
-
-isc_result_t
-isc_rwlock_tryupgrade(isc_rwlock_t *rwl);
 
 void
 isc_rwlock_downgrade(isc_rwlock_t *rwl);
@@ -101,5 +67,3 @@ void
 isc_rwlock_destroy(isc_rwlock_t *rwl);
 
 ISC_LANG_ENDDECLS
-
-#endif /* ISC_RWLOCK_H */
