2f9e77b2ee2f82069be2dad7d7cf6ba1603d7e90
Witold Kręcicki <wpk@isc.org>
2020-02-07 12:06:03 +0100

    fixup! Make client resolver tasks pooled and CPU-bound

--- lib/ns/client.c
+++ lib/ns/client.c
@@ -2236,7 +2236,7 @@ get_clienttask(ns_clientmgr_t *manager, isc_task_t **taskp) {
 		tid = isc_random_uniform(manager->ncpus);
 	}
 
-	int rand = isc_random_uniform(CLIENT_NMTASKS_PERCPU);
+	int rand = isc_random_uniform(CLIENT_NTASKS_PERCPU);
 	int nexttask = (rand * manager->ncpus) + tid;
 	isc_task_attach(manager->taskpool[nexttask], taskp);
 }
