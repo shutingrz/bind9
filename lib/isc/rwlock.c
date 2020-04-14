c1f1a17379f49c1dce4aaa87480693eb30a4d4db
Witold Kręcicki <wpk@isc.org>
2020-04-14 13:22:36 +0200

    Experiment 2 - use PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP

--- lib/isc/rwlock.c
+++ lib/isc/rwlock.c
@@ -36,7 +36,9 @@ isc_rwlock_init(isc_rwlock_t *rwl, unsigned int read_quota,
 		unsigned int write_quota) {
 	UNUSED(read_quota);
 	UNUSED(write_quota);
-	REQUIRE(pthread_rwlock_init(&rwl->rwlock, NULL) == 0);
+	pthread_rwlockattr_t attr;
+	pthread_rwlockattr_setkind_np(&attr, PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
+	REQUIRE(pthread_rwlock_init(&rwl->rwlock, &attr) == 0);
 	atomic_init(&rwl->downgrade, false);
 	return (ISC_R_SUCCESS);
 }
